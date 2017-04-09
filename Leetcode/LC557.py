afa0d42341487fae97fd4da7dcd7c32455ef538d
zbgzbg2007 <zbgzbg2007@gmail.com>
2017-04-09 11:53:39 -0700

    Create LC557.py

---
+++ Leetcode/LC557.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        return " ".join([x[::-1] for x in s.split()])
